36caaad5ea761cb5d0d946147ddb35e777dc246f
Nic Dobbins <ndobb@uw.edu>
2020-11-06 17:46:56 -0800

    Simplified sqlset adding

--- omop/5.3/bootstrap_omop.sql
+++ omop/5.3/bootstrap_omop.sql
@@ -16,18 +16,8 @@ DECLARE @add_labs         BIT = 1
 DECLARE @add_meds         BIT = 1
 DECLARE @add_vitals       BIT = 1
 
-DECLARE @yes INT = 1
-DECLARE @no  INT = 0
-
-DECLARE @sqlset_person               INT
-DECLARE @sqlset_visit_occurrence     INT
-DECLARE @sqlset_condition_occurrence INT
-DECLARE @sqlset_v_death              INT
-DECLARE @sqlset_device_exposure      INT
-DECLARE @sqlset_drug_exposure        INT
-DECLARE @sqlset_measurement          INT
-DECLARE @sqlset_observation          INT
-DECLARE @sqlset_procedure_occurrence INT
+DECLARE @yes BIT = 1
+DECLARE @no  BIT = 0
 
 DECLARE @user NVARCHAR(20) = 'bootstrap_omop.sql'
 
@@ -35,57 +25,28 @@ DECLARE @user NVARCHAR(20) = 'bootstrap_omop.sql'
  * Create metadata temp table of all OMOP 
  * SQL Sets to be queried in Leaf.
  */
-BEGIN TRY DROP TABLE #SqlSets END TRY BEGIN CATCH END CATCH
-BEGIN TRY DROP TABLE #SqlSetsInput END TRY BEGIN CATCH END CATCH
+INSERT INTO LeafDB.app.ConceptSqlSet (SqlSetFrom, IsEncounterBased, IsEventBased, SqlFieldDate, Created, CreatedBy, Updated, UpdatedBy)
+SELECT *
+FROM (VALUES ('dbo.person',               @no,  @no,  NULL,                              GETDATE(), @user, GETDATE(), @user),                          
+             ('dbo.visit_occurrence',     @yes, @no, '@.visit_start_datetime',           GETDATE(), @user, GETDATE(), @user),
+             ('dbo.condition_occurrence', @yes, @no, '@.condition_start_datetime',       GETDATE(), @user, GETDATE(), @user),
+             ('dbo.v_death',              @yes, @no, '@.death_datetime',                 GETDATE(), @user, GETDATE(), @user),          
+             ('dbo.device_exposure',      @yes, @no, '@.device_exposure_start_datetime', GETDATE(), @user, GETDATE(), @user),  
+             ('dbo.drug_exposure',        @yes, @no, '@.drug_exposure_start_datetime',   GETDATE(), @user, GETDATE(), @user),
+             ('dbo.measurement',          @yes, @no, '@.measurement_datetime',           GETDATE(), @user, GETDATE(), @user), 
+             ('dbo.observation',          @yes, @no, '@.observation_datetime',           GETDATE(), @user, GETDATE(), @user),      
+             ('dbo.procedure_occurrence', @yes, @no, '@.procedure_datetime',             GETDATE(), @user, GETDATE(), @user)
+     ) AS X(col1,col2,col3,col4,col5,col6,col7,col8)
 
-CREATE TABLE #SqlSets      (Id INT, SqlSetName NVARCHAR(50), IsEncounterBased BIT)
-CREATE TABLE #SqlSetsInput (SqlSetName NVARCHAR(50), IsEncounterBased BIT, SqlFieldDate NVARCHAR(50), TempId INT)
-
-INSERT INTO  #SqlSetsInput (SqlSetName, IsEncounterBased, SqlFieldDate, TempId)
-SELECT col1, col2, col3, ROW_NUMBER() OVER(ORDER BY (SELECT 1))
-FROM (VALUES ('dbo.person',               @no,  NULL),                          
-             ('dbo.visit_occurrence',     @yes, '@.visit_start_datetime'),
-             ('dbo.condition_occurrence', @yes, '@.condition_start_datetime'),
-             ('dbo.v_death',              @yes, '@.death_datetime'),          
-             ('dbo.device_exposure',      @yes, '@.device_exposure_start_datetime'),  
-             ('dbo.drug_exposure',        @yes, '@.drug_exposure_start_datetime'),
-             ('dbo.measurement',          @yes, '@.measurement_datetime'), 
-             ('dbo.observation',          @yes, '@.observation_datetime'),      
-             ('dbo.procedure_occurrence', @yes, '@.procedure_datetime')
-     ) AS X(col1,col2,col3)
-
-/**
- * For each OMOP table of interest, INSERT into
- * the ConceptSqlSet table and track the output ID.
- */
-DECLARE @row_id    INT = 1
-DECLARE @row_count INT = (SELECT COUNT(*) FROM #SqlSetsInput)
-DECLARE @set_name NVARCHAR(50)
-DECLARE @is_encounter_based BIT
-DECLARE @set_field_date NVARCHAR(50)
-
-WHILE @row_id <= @row_count
-BEGIN
-    SELECT @set_name = X.SqlSetName, @is_encounter_based = X.IsEncounterBased, @set_field_date = X.SqlFieldDate
-    FROM #SqlSetsInput AS X
-    WHERE TempId = @row_id
-
-    INSERT INTO LeafDB.app.ConceptSqlSet (IsEncounterBased, IsEventBased, SqlSetFrom, SqlFieldDate, Created, CreatedBy, Updated, UpdatedBy)
-    OUTPUT INSERTED.Id, @set_name, @is_encounter_based INTO #SqlSets (Id, SqlSetName, IsEncounterBased)
-    SELECT @is_encounter_based, 0, @set_name, @set_field_date, GETDATE(), @user, GETDATE(), @user
-
-    SET @row_id += 1
-END
-
-SET @sqlset_person               = (SELECT TOP 1 Id FROM #SqlSets WHERE SqlSetName = 'dbo.person')
-SET @sqlset_visit_occurrence     = (SELECT TOP 1 Id FROM #SqlSets WHERE SqlSetName = 'dbo.visit_occurrence')  
-SET @sqlset_condition_occurrence = (SELECT TOP 1 Id FROM #SqlSets WHERE SqlSetName = 'dbo.condition_occurrence')  
-SET @sqlset_v_death              = (SELECT TOP 1 Id FROM #SqlSets WHERE SqlSetName = 'dbo.v_death')  
-SET @sqlset_device_exposure      = (SELECT TOP 1 Id FROM #SqlSets WHERE SqlSetName = 'dbo.device_exposure')  
-SET @sqlset_drug_exposure        = (SELECT TOP 1 Id FROM #SqlSets WHERE SqlSetName = 'dbo.drug_exposure')  
-SET @sqlset_measurement          = (SELECT TOP 1 Id FROM #SqlSets WHERE SqlSetName = 'dbo.measurement')  
-SET @sqlset_observation          = (SELECT TOP 1 Id FROM #SqlSets WHERE SqlSetName = 'dbo.observation')  
-SET @sqlset_procedure_occurrence = (SELECT TOP 1 Id FROM #SqlSets WHERE SqlSetName = 'dbo.procedure_occurrence')  
+DECLARE @sqlset_person               INT = (SELECT TOP 1 Id FROM LeafDB.app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.person')
+DECLARE @sqlset_visit_occurrence     INT = (SELECT TOP 1 Id FROM LeafDB.app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.visit_occurrence')  
+DECLARE @sqlset_condition_occurrence INT = (SELECT TOP 1 Id FROM LeafDB.app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.condition_occurrence')  
+DECLARE @sqlset_v_death              INT = (SELECT TOP 1 Id FROM LeafDB.app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.v_death')  
+DECLARE @sqlset_device_exposure      INT = (SELECT TOP 1 Id FROM LeafDB.app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.device_exposure')  
+DECLARE @sqlset_drug_exposure        INT = (SELECT TOP 1 Id FROM LeafDB.app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.drug_exposure')  
+DECLARE @sqlset_measurement          INT = (SELECT TOP 1 Id FROM LeafDB.app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.measurement')  
+DECLARE @sqlset_observation          INT = (SELECT TOP 1 Id FROM LeafDB.app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.observation')  
+DECLARE @sqlset_procedure_occurrence INT = (SELECT TOP 1 Id FROM LeafDB.app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.procedure_occurrence')  
 
 /**
  * Demographics

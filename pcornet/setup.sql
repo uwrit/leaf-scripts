077786c0ef7f089ec79ffd83a0631d7a691aa1c7
Nic Dobbins <ndobb@uw.edu>
2023-02-23 16:12:40 -0800

    Added Ross's code for CPT, ICD10PCS

--- pcornet/setup.sql
+++ pcornet/setup.sql
@@ -1,4 +1,7 @@
 /**
+ *
+ * Authors: Nic Dobbins, Ross Naheedy
+ *
  * The script auto-populates the Leaf concept tree with PCORNet-specific fields and tables.
  * PCORNET uses the fields `PATID` and `ENCOUNTERID` to track patient and encounter identifiers.
  * This script thus assumes Leaf appsettings.json Compiler.FieldPersonId and Compiler.FieldEncounterId
@@ -10,8 +13,8 @@
  *
  * Appsettings file: https://github.com/uwrit/leaf/blob/master/src/server/API/appsettings.json#L65
  * Config instructions: https://leafdocs.rit.uw.edu/installation/installation_steps/6_appsettings/#compiler
- */
-
+ *
+ *
 /**
  * Cheatsheet code to clear the current concept tree:
 
@@ -456,6 +459,224 @@ FROM #icd10_2 AS X
 WHERE ParentAUI IS NOT NULL
 
 /** 
+ * Procedures - CPT
+ * Note: driven from cpt.sql available at https://drive.google.com/drive/u/0/folders/1GRtL04PHBz8OAHRZB455mc1qAiCfU1pL. 
+ */
+DECLARE @sqlset_pxcpt INT = (SELECT TOP 1 Id FROM app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.PROCEDURES')
+
+DECLARE @uid_pxcpt_root  NVARCHAR(50) = 'urn:leaf:concept:px'
+
+/** 
+ * Create a temporary table for transforming CPT
+ */
+BEGIN TRY DROP TABLE #cpt END TRY BEGIN CATCH END CATCH
+BEGIN TRY DROP TABLE #cpt_2 END TRY BEGIN CATCH END CATCH
+SELECT  
+     AUI
+    ,ParentAUI
+    ,[ParentUniversalId] = 'urn:leaf:concept:px:cpt:' + (SELECT TOP 1 CASE WHEN CodeCount = 1 THEN MinCode ELSE MinCode + '_' + MaxCode END 
+                                                          FROM [TestDB].dbo.[UMLS_CPT] AS X2
+                                                          WHERE X.ParentAUI = X2.AUI)
+    ,[UniversalId]      = 'urn:leaf:concept:px:cpt:' + CASE WHEN CodeCount = 1 THEN MinCode ELSE MinCode + '_' + MaxCode END
+    ,[IsParent]         = CASE WHEN CodeCount = 1 THEN 0 ELSE 1 END
+    ,[SqlSetWhere]      = '@.PX_TYPE = ''CH'' AND @.PX ' + [SqlSetWhere]
+    ,[UiDisplayName]
+    ,[UiDisplayText]    = 'Had the procedure ' + [UiDisplayName]
+INTO #cpt    
+FROM [TestDB].dbo.[UMLS_CPT] AS X
+
+/** 
+ * Using this UniversalId naming convention, occassionally CPT code ranges can be duplicated.
+ * Use a CTE with ROW_NUMBER() to identify cases of dupes. We'll add a count to the end of each 
+ * duplicated UniversalId to de-dupe them
+ */
+; WITH cpt AS
+(
+     SELECT 
+          AUI, ParentAUI, UniversalId, SqlSetWhere, UiDisplayName, UiDisplayText, IsParent, 
+          RowNum = ROW_NUMBER() OVER (PARTITION BY UniversalId ORDER BY UniversalId)
+     FROM #cpt
+), cpt_2 AS
+(
+     SELECT 
+          AUI
+        , ParentAUI
+        , UniversalId = CASE WHEN RowNum > 1 THEN UniversalId + '_' + CONVERT(NVARCHAR(2), RowNum) ELSE UniversalId END
+        , SqlSetWhere
+        , UiDisplayName
+        , UiDisplayText
+        , IsParent
+     FROM cpt
+)
+SELECT * 
+INTO #cpt_2 
+FROM cpt_2
+
+INSERT INTO #concepts (ExternalId, ExternalParentId, UniversalId, [IsNumeric], IsParent, IsRoot, SqlSetId, SqlSetWhere, 
+                       SqlFieldNumeric, UiDisplayName, UiDisplayText, UiDisplayUnits, UiNumericDefaultText)
+
+/* Procedures root */
+SELECT ExternalId            = @uid_pxcpt_root
+     , ExternalParentId      = NULL
+     , UniversalId           = @uid_pxcpt_root
+     , [IsNumeric]           = @no
+     , IsParent              = @yes
+     , IsRoot                = @yes
+     , SqlSetId              = @sqlset_pxcpt
+     , SqlSetWhere           = NULL
+     , SqlFieldNumeric       = NULL
+     , UiDisplayName         = 'Procedures'
+     , UiDisplayText         = 'Have had a procedure'
+     , UiDisplayUnits        = NULL
+     , UiNumericDefaultText  = NULL
+     
+UNION ALL 
+
+/* Procedures - CPT root */ 
+SELECT ExternalId            = (SELECT TOP 1 UniversalId FROM #cpt_2 WHERE ParentAUI IS NULL)
+     , ExternalParentId      = @uid_pxcpt_root
+     , UniversalId           = (SELECT TOP 1 UniversalId FROM #cpt_2 WHERE ParentAUI IS NULL)
+     , [IsNumeric]           = @no
+     , IsParent              = @yes
+     , IsRoot                = @no
+     , SqlSetId              = @sqlset_pxcpt
+     , SqlSetWhere           = NULL
+     , SqlFieldNumeric       = NULL
+     , UiDisplayName         = 'CPT'
+     , UiDisplayText         = 'Have had a procedure coded in CPT'
+     , UiDisplayUnits        = NULL
+     , UiNumericDefaultText  = NULL
+     
+UNION ALL 
+
+/* Procedures - CPT codes */ 
+SELECT ExternalId            = X.UniversalId
+     , ExternalParentId      = (SELECT TOP 1 UniversalId FROM #cpt_2 AS X2 WHERE X.ParentAUI = X2.AUI)
+     , UniversalId           = X.UniversalId
+     , [IsNumeric]           = @no
+     , IsParent              = X.IsParent
+     , IsRoot                = @no
+     , SqlSetId              = @sqlset_pxcpt
+     , SqlSetWhere           = X.SqlSetWhere
+     , SqlFieldNumeric       = NULL
+     , UiDisplayName         = Left(X.UiDisplayName,400)
+     , UiDisplayText         = X.UiDisplayText
+     , UiDisplayUnits        = NULL
+     , UiNumericDefaultText  = NULL
+FROM #cpt_2 AS X    
+WHERE ParentAUI IS NOT NULL
+
+/** 
+ * Procedures - ICD10PCS
+ * Note: driven from icd10pcs.sql available at https://drive.google.com/drive/u/0/folders/1GRtL04PHBz8OAHRZB455mc1qAiCfU1pL. 
+ */
+DECLARE @sqlset_pxicd INT = (SELECT TOP 1 Id FROM app.ConceptSqlSet WHERE SqlSetFrom = 'dbo.PROCEDURES')
+
+DECLARE @uid_pxicd_root  NVARCHAR(50) = 'urn:leaf:concept:px'
+
+/** 
+ * Create a temporary table for transforming ICD10PCS
+ */
+BEGIN TRY DROP TABLE #icd10pcs END TRY BEGIN CATCH END CATCH
+BEGIN TRY DROP TABLE #icd10pcs_2 END TRY BEGIN CATCH END CATCH
+SELECT  
+     AUI
+    ,ParentAUI
+    ,[ParentUniversalId] = 'urn:leaf:concept:px:icd10pcs:' + (SELECT TOP 1 CASE WHEN CodeCount = 1 THEN MinCode ELSE MinCode + '_' + MaxCode END 
+                                                          FROM [TestDB].dbo.[UMLS_ICD10PCS] AS X2
+                                                          WHERE X.ParentAUI = X2.AUI)
+    ,[UniversalId]      = 'urn:leaf:concept:px:icd10pcs:' + CASE WHEN CodeCount = 1 THEN MinCode ELSE MinCode + '_' + MaxCode END
+    ,[IsParent]         = CASE WHEN CodeCount = 1 THEN 0 ELSE 1 END
+    ,[SqlSetWhere]      = '@.PX_TYPE = ''10'' AND @.PX ' + [SqlSetWhere]
+    ,[UiDisplayName]
+    ,[UiDisplayText]    = 'Had the procedure ' + [UiDisplayName]
+INTO #icd10pcs    
+FROM [TestDB].dbo.[UMLS_ICD10PCS] AS X
+
+/** 
+ * Using this UniversalId naming convention, occassionally ICD10PCS code ranges can be duplicated.
+ * Use a CTE with ROW_NUMBER() to identify cases of dupes. We'll add a count to the end of each 
+ * duplicated UniversalId to de-dupe them
+ */
+; WITH icd10pcs AS
+(
+     SELECT 
+          AUI, ParentAUI, UniversalId, SqlSetWhere, UiDisplayName, UiDisplayText, IsParent, 
+          RowNum = ROW_NUMBER() OVER (PARTITION BY UniversalId ORDER BY UniversalId)
+     FROM #icd10pcs
+), icd10pcs_2 AS
+(
+     SELECT 
+          AUI
+        , ParentAUI
+        , UniversalId = CASE WHEN RowNum > 1 THEN UniversalId + '_' + CONVERT(NVARCHAR(2), RowNum) ELSE UniversalId END
+        , SqlSetWhere
+        , UiDisplayName
+        , UiDisplayText
+        , IsParent
+     FROM icd10pcs
+)
+SELECT * 
+INTO #icd10pcs_2 
+FROM icd10pcs_2
+
+INSERT INTO #concepts (ExternalId, ExternalParentId, UniversalId, [IsNumeric], IsParent, IsRoot, SqlSetId, SqlSetWhere, 
+                       SqlFieldNumeric, UiDisplayName, UiDisplayText, UiDisplayUnits, UiNumericDefaultText)
+
+/* Procedures - ICD10PCS root */ 
+SELECT ExternalId            = 'urn:leaf:concept:px:icd10pcs'
+     , ExternalParentId      = @uid_pxicd_root
+     , UniversalId           = 'urn:leaf:concept:px:icd10pcs'
+     , [IsNumeric]           = @no
+     , IsParent              = @yes
+     , IsRoot                = @no
+     , SqlSetId              = @sqlset_pxicd
+     , SqlSetWhere           = NULL
+     , SqlFieldNumeric       = NULL
+     , UiDisplayName         = 'ICD10PCS'
+     , UiDisplayText         = 'Have had a procedure coded in ICD10PCS'
+     , UiDisplayUnits        = NULL
+     , UiNumericDefaultText  = NULL
+     
+UNION ALL 
+
+/* Procedures - ICD10PCS codes */ 
+SELECT ExternalId            = X.UniversalId
+     , ExternalParentId      = 'urn:leaf:concept:px:icd10pcs' -- (SELECT UniversalId FROM #icd10pcs_2 AS X2 WHERE X2.ParentAUI is null)
+     , UniversalId           = X.UniversalId
+     , [IsNumeric]           = @no
+     , IsParent              = X.IsParent
+     , IsRoot                = @no
+     , SqlSetId              = @sqlset_pxicd
+     , SqlSetWhere           = X.SqlSetWhere
+     , SqlFieldNumeric       = NULL
+     , UiDisplayName         = Left(X.UiDisplayName,400)
+     , UiDisplayText         = X.UiDisplayText
+     , UiDisplayUnits        = NULL
+     , UiNumericDefaultText  = NULL
+FROM #icd10pcs_2 AS X    
+WHERE ParentAUI = 'A16077350'
+
+UNION ALL
+
+/* Procedures - ICD10PCS codes */ 
+SELECT ExternalId            = X.UniversalId
+     , ExternalParentId      = (SELECT TOP 1 UniversalId FROM #icd10pcs_2 AS X2 WHERE X.ParentAUI = X2.AUI)
+     , UniversalId           = X.UniversalId
+     , [IsNumeric]           = @no
+     , IsParent              = X.IsParent
+     , IsRoot                = @no
+     , SqlSetId              = @sqlset_pxicd
+     , SqlSetWhere           = X.SqlSetWhere
+     , SqlFieldNumeric       = NULL
+     , UiDisplayName         = Left(X.UiDisplayName,400)
+     , UiDisplayText         = X.UiDisplayText
+     , UiDisplayUnits        = NULL
+     , UiNumericDefaultText  = NULL
+FROM #icd10pcs_2 AS X    
+WHERE ParentAUI IS NOT NULL
+
+/** 
  * Final steps: Add to Leaf
  */
 INSERT INTO app.Concept ([ExternalId],[ExternalParentId],[UniversalId],[IsPatientCountAutoCalculated],[IsNumeric],[IsParent],[IsRoot],
@@ -521,3 +742,6 @@ SET RootId = roots.RootId
 FROM app.Concept AS C
      INNER JOIN roots ON C.Id = roots.Id
 WHERE C.RootId IS NULL
+
+
+
